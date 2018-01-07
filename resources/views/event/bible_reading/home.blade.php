1301bd7b64739967c84b771fd9f21067c4ad5d71
Elliot Yap <yilliot@gmail.com>
2018-01-08 05:53:26 +0800

    move fb script to home top only

--- resources/views/event/bible_reading/home.blade.php
+++ resources/views/event/bible_reading/home.blade.php
@@ -5,6 +5,15 @@ Bible Reading Home
 @endsection
 
 @section('content')
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.11&appId=284805762045136';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
   |
   @if(!session('nric'))
   <a href="/event/bible_reading/signup"> {{trans('event.bible_reading.signup')}} </a> |
