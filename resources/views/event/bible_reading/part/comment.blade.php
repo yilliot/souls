e4d15037e40cf05a856caadfcf8dfa85162e8e6e
Khoo Yong Jun <khoo.jack@gmail.com>
2018-01-07 20:07:22 +0800

    added facebook like plugin

--- resources/views/event/bible_reading/part/comment.blade.php
+++ resources/views/event/bible_reading/part/comment.blade.php
@@ -13,6 +13,7 @@
     </div>
   </div>
   <div class="extra content">
+    <div class="fb-like" data-href="http://hcc.ground.my/bible_reading/?comment={{$comment->id}}" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
     <div class="right floated author">
       <i class="user icon"></i>
        {{ $comment->soul()->first()->nickname }}
