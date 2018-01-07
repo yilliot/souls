d577a462cd021355c8e92907b0d4841276f34dab
Khoo Yong Jun <khoo.jack@gmail.com>
2018-01-07 23:39:03 +0800

    remove potential query growth

--- app/Http/Controllers/Event/BibleReading/BibleReadingSupervision.php
+++ app/Http/Controllers/Event/BibleReading/BibleReadingSupervision.php
@@ -26,19 +26,19 @@ class BibleReadingSupervision extends Controller
   	$souls = Soul::where('cellgroup_id', $cellgroup_id)->get();
     if($souls->isNotEmpty()) {
       $check_ins = CheckIn::whereIn('soul_id', $souls->pluck('id'))->get();
+      $CheckInChapter = CheckInChapter::whereIn('check_in_id', $check_ins->pluck('id'))->get();
       foreach($souls as $soul) {
         $soul_checkins = $check_ins->where('soul_id', $soul->id);
-        $soul['count'] = $this->countRecord($soul_checkins);
+        $soul['count'] = $this->countRecord($soul_checkins, $CheckInChapter);
       }
     }
 
   	return view('event.bible_reading.supervision.show_souls', compact('souls'));
   }
 
-  public function countRecord($check_in)
+  public function countRecord($check_in, $checkInChapter)
   {
       $check_in_chapter = [];
-      $checkInChapter = CheckInChapter::whereIn('check_in_id', $check_in->pluck('id'))->get();
       foreach ($check_in as $checkIn) {
         $check_in_chapter[] = $checkInChapter->where('check_in_id', $checkIn->id);
       }
