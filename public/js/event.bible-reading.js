f3ecb9982086affcb64409f4fdbf571f172f6676
davion111 <30561962+davion111@users.noreply.github.com>
2018-03-14 14:22:38 +0800

    pull develop take latest file

---
+++ public/js/event.bible-reading.js
@@ -0,0 +1,18 @@
+webpackJsonp([3],{
+
+/***/ 135:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(136);
+
+
+/***/ }),
+
+/***/ 136:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'C:\\wamp64\\www\\souls\\resources\\assets\\js\\event.bible-reading.js'");
+
+/***/ })
+
+},[135]);
---
+++ public/js/event.supreme-vote.js
@@ -0,0 +1,18 @@
+webpackJsonp([4],{
+
+/***/ 133:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(134);
+
+
+/***/ }),
+
+/***/ 134:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open 'C:\\wamp64\\www\\souls\\resources\\assets\\js\\event.supreme-vote.js'");
+
+/***/ })
+
+},[133]);
