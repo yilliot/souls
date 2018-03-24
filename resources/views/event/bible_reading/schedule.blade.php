31018cb0287f1b228c23c00b2f3c3186284dd6d8
Chloe <chloe.lishien@gmail.com>
2018-03-25 00:39:43 +0800

    bible reading schedule April

--- resources/views/event/bible_reading/schedule.blade.php
+++ resources/views/event/bible_reading/schedule.blade.php
@@ -18,13 +18,13 @@ Bible Reading Home
   @include('event.bible_reading.part.logo')
   @include('event.bible_reading.part.flash')
   <style>
-    td{text-align: center !important; padding: 1px !important;}
-    th{text-align: center !important;}
-    td label{display: block;}
-    td span{display:block; font-size: 0.8em;}
+    #schedule td{text-align: center; padding: 1px;}
+    #schedule th{text-align: center;}
+    #schedule td label{display: block;}
+    #schedule td span{display:block; font-size: 0.8em;}
   </style>
-  <h3 class="ui header">三月读经表</h3>
-  <table class="ui unstackable compact table">
+  <h3 class="ui header">四月读经表</h3>
+  <table id="schedule" class="ui unstackable compact table">
     <thead>
       <tr>
         <th>Mon</th>
@@ -39,46 +39,54 @@ Bible Reading Home
         <td><label></label> <span></span></td>
         <td><label></label> <span></span></td>
         <td><label></label> <span></span></td>
-        <td><label>申/书</label> <span>34-2</span></td>
-        <td><label>书</label> <span>3-5</span></td>
-        <td><label>书</label> <span>6-8</span></td>
-        <td><label>书</label> <span>9-13</span></td>
+        <td><label></label> <span></span></td>
+        <td><label></label> <span></span></td>
+        <td colspan="2" style="text-align: right;">
+          <label>撒下／王上</label> 
+          <span style="padding-right: 17px ">21-1</span>
+        </td>
       </tr>
       <tr>
-        <td><label>书</label> <span>14-16</span></td>
-        <td><label>书</label> <span>17-19</span></td>
-        <td><label>书</label> <span>20-22</span></td>
-        <td><label>书/士</label> <span>23-1</span></td>
-        <td><label>士</label> <span>2-4</span></td>
-        <td><label>士</label> <span>5-7</span></td>
-        <td><label>士</label> <span>8-12</span></td>
+        <td><label>王上</label> <span>2-4</span></td>
+        <td><label>王上</label> <span>5-7</span></td>
+        <td><label>王上</label> <span>8-10</span></td>
+        <td><label>王上</label> <span>11-13</span></td>
+        <td><label>王上</label> <span>14-16</span></td>
+        <td><label>王上</label> <span>17-19</span></td>
+        <td><label>王上/下</label> <span>20-2</span></td>
       </tr>
       <tr>
-        <td><label>士</label> <span>13-15</span></td>
-        <td><label>士</label> <span>16-18</span></td>
-        <td><label>士</label> <span>19-21</span></td>
-        <td><label>得</label> <span>1-3</span></td>
-        <td><label>得/撒上</label> <span>4-2</span></td>
-        <td><label>撒上</label> <span>3-5</span></td>
-        <td><label>撒上</label> <span>6-10</span></td>
+        <td><label>王下</label> <span>3-5</span></td>
+        <td><label>王下</label> <span>6-8</span></td>
+        <td><label>王下</label> <span>9-11</span></td>
+        <td><label>王下</label> <span>12-14</span></td>
+        <td><label>王下</label> <span>15-17</span></td>
+        <td><label>王下</label> <span>18-20</span></td>
+        <td><label>王下</label> <span>21-25</span></td>
       </tr>
       <tr>
-        <td><label>撒上</label> <span>11-13</span></td>
-        <td><label>撒上</label> <span>14-16</span></td>
-        <td><label>撒上</label> <span>17-19</span></td>
-        <td><label>撒上</label> <span>20-22</span></td>
-        <td><label>撒上</label> <span>23-25</span></td>
-        <td><label>撒上</label> <span>26-28</span></td>
-        <td><label>撒上/撒下</label> <span>29-2</span></td>
+        <td><label>代上</label> <span>1-3</span></td>
+        <td><label>代上</label> <span>4-6</span></td>
+        <td><label>代上</label> <span>7-9</span></td>
+        <td><label>代上</label> <span>10-12</span></td>
+        <td><label>代上</label> <span>13-15</span></td>
+        <td><label>代上</label> <span>16-18</span></td>
+        <td><label>代上</label> <span>19-23</span></td>
       </tr>
       <tr>
-        <td><label>撒下</label> <span>3-5</span></td>
-        <td><label>撒下</label> <span>6-8</span></td>
-        <td><label>撒下</label> <span>9-11</span></td>
-        <td><label>撒下</label> <span>12-14</span></td>
-        <td><label>撒下</label> <span>15-17</span></td>
-        <td><label>撒下</label> <span>18-20</span></td>
+        <td><label>代上</label> <span>24-26</span></td>
+        <td><label>代上</label> <span>27-29</span></td>
+        <td><label>代下</label> <span>1-3</span></td>
+        <td><label>代下</label> <span>4-6</span></td>
+        <td><label>代下</label> <span>7-9</span></td>
+        <td><label>代下</label> <span>10-12</span></td>
+        <td><label>代下</label> <span>13-17</span></td>
       </tr>
+      <tr>
+        <td><label>代下</label> <span>18-20</span></td>
+      </tr>
+       
+
 
     </thead>
     
