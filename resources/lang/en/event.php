b6f1b9987d765ed305ff1acb754e2852eb123df2
davion111 <30561962+davion111@users.noreply.github.com>
2018-04-24 23:34:57 +0800

    language translation

--- resources/lang/en/event.php
+++ resources/lang/en/event.php
@@ -39,7 +39,15 @@ return [
         //newcomer list
         'assigned_people' => 'Assigned People',
         'assigned_cg' => 'Assigned Cell Group',
-        'newcomer_list' => 'Newcomer List'
+        'newcomer_list' => 'Newcomer List',
+        'phone_number' => 'Phone Number',
+        'inviter' => 'Inviter',
+        'profile_picture' => 'Profile Picture', 
+        'birthday' => 'Birthday',
+        'christian' => 'Christian',
+        'fbid' => 'FBID',
+        'about_me' => 'Description',
+        'submit' => 'Submit',
     ],
     
 
--- resources/lang/zh/event.php
+++ resources/lang/zh/event.php
@@ -40,7 +40,15 @@ return [
         //newcomer list
         'assigned_people' => '分配人',
         'assigned_cg' => '分配小组',
-        'newcomer_list' => '新人名单'
+        'newcomer_list' => '新人名单',
+        'phone_number' => '电话号码',
+        'inviter' => '邀请者',
+        'profile_picture' => '个人信息', 
+        'birthday' => '生日日期',
+        'christian' => '基督徒',
+        'fbid' => '脸书帐号',
+        'about_me' => '关于自己',
+        'submit' => '储存'
     ],
     
     'bible_reading' => [
