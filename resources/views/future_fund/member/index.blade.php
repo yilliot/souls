5e793d71ba40909f824d8c48216fee97f0256501
Elliot Yap <yilliot@gmail.com>
2018-12-18 22:07:32 +0800

    ff index

--- resources/views/future_fund/member/index.blade.php
+++ resources/views/future_fund/member/index.blade.php
@@ -5,15 +5,19 @@ Future Fund
 @endsection
 
 @section('content')
-<div style="padding-top: 50px"></div>
-RM {{$session_collected-$session_total}} to go
+<h1 id="header" class="header bg-size">距离目标尚差 <br> RM {{number_format($session_total-$session_collected, 2)}} </h1>
+<div class="bg-size bg-image bg-image-fade"></div>
+<div class="bg-size bg-image" style="width:{{$session_collected/$session_total*100}}%"></div>
+<div style="padding-top: 200px"></div>
+<div style="clear:both"></div>
+
 <div style="float:left">
-  <h4 class="ui header">Collected Amount</h4>
-  <div>RM {{$session_collected}}</div>
+  <h4 class="ui header">已收 Collected Amount</h4>
+  <div>RM {{number_format($session_collected, 2)}}</div>
 </div>
 <div style="float:right; text-align: right">
-  <h4 class="ui header">Total Pledge</h4>
-  <div>RM {{$session_total}}</div>
+  <h4 class="ui header">总数 Total Pledge</h4>
+  <div>RM {{number_format($session_total, 2)}}</div>
 </div>
 <div style="clear:both"></div>
 <div class="ui indicating progress" data-percent="{{$session_collected/$session_total*100}}">
@@ -22,6 +26,7 @@ RM {{$session_collected-$session_total}} to go
   </div>
   <div class="label">{{$session->name}}</div>
 </div>
-<div style="padding-top: 50px"></div>
+<div style="padding-top: 10px"></div>
+
 
 @endsection
