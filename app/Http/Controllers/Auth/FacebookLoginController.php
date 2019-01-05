334fe47f97aacb462604d1ed508711cf6652dfe3
Elliot Yap <yilliot@gmail.com>
2019-01-05 13:59:07 +0800

    request name space

--- app/Http/Controllers/Auth/FacebookLoginController.php
+++ app/Http/Controllers/Auth/FacebookLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Socialite;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class FacebookLoginController extends Controller
 {
