a178e53989926c53ab8ea273af08c72859209013
Elliot Yap <yilliot@gmail.com>
2019-04-08 14:18:59 +0800

    rename for cache clearing

--- resources/views/future_fund/member/layout.blade.php
+++ resources/views/future_fund/member/layout.blade.php
@@ -7,7 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title> @yield('title') : {{ config('app.name') }}</title>
   <link href="{{ asset('semantic/semantic.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/future-fund.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/future-fund.css') }}?1904" rel="stylesheet">
 </head>
 <body>
   @yield('pre-body')
